d7cda7dca56b83ac533fd5f80435476ce870f3da
Boris Popadiuk <pboris@ada.cs.pdx.edu>
2018-04-27 13:01:34 -0700

    increased WINDOW_SIZE to 4096 for increased frequency resolution -- now works down to 130 Hz, don't know if this works in practice...

--- working_goertzel.py
+++ working_goertzel.py
@@ -3,7 +3,7 @@ import numpy as np
 from ChordNaming import name
 
 SAMPLE_RATE = 44100
-WINDOW_SIZE = 1024
+WINDOW_SIZE = 4096 
 
 def goertzel_mag(numSamples, targetFreqs, sampRate, data):
     scalingFactor = numSamples / 2.0
@@ -33,19 +33,28 @@ def goertzel_mag(numSamples, targetFreqs, sampRate, data):
     return magnitudes 
 
 t = np.linspace(0, 1, SAMPLE_RATE)[:WINDOW_SIZE]
-sine_wave = np.sin(2*np.pi*1318*t) + np.sin(2*np.pi*1396*t) + np.sin(2*np.pi*1760*t) + np.sin(2*np.pi*1046*t)# should be 'F Major 7'
+#sine_wave = np.sin(2*np.pi*440*t) + np.sin(2*np.pi*392*t) + np.sin(2*np.pi*261*t) + np.sin(2*np.pi*329*t)# should be 'F Major 7'
+#sine_wave = np.sin(2*np.pi*1046*t) + np.sin(2*np.pi*1318*t) + np.sin(2*np.pi*1567*t) + np.sin(2*np.pi*1975*t)# should be 'C Major 7'
+sine_wave = np.sin(2*np.pi*233.08*t) + np.sin(2*np.pi*207.65*t) + np.sin(2*np.pi*138.59*t) + np.sin(2*np.pi*174.61*t)# should be 'Bb minor 7'
 sine_wave = sine_wave * np.hamming(WINDOW_SIZE)
 
-freqs = [1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53]
+freqs0 = [261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.3, 440.00, 466.16, 493.88]
+freqs1 = [1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53]
+freqs2 = [130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94]
+freqs = freqs0 + freqs1 + freqs2
+
 mags = goertzel_mag(WINDOW_SIZE, freqs, SAMPLE_RATE, sine_wave)
 
-#mags.sort(key=lambda x : x[1], reverse=True)
+mags.sort(key=lambda x : x[1], reverse=True)
 
 chord = set()
-magRange = max(mags, key=lambda x: x[1])[1] - min(mags, key=lambda x: x[1])[1]
+magMin = min(mags, key=lambda x: x[1])[1]
+magMax = max(mags, key=lambda x: x[1])[1]
+magRange = magMax - magMin 
 for note, mag in mags:
-    if mag > magRange / 2:
+    if mag > magMax * 0.8:
         chord.add(note)
 
+print('mags: \n', mags)
 chord = list(chord)
 name.process(chord)
