2a451fa783e238035d95928f2f51c035991ec04f
Boris Popadiuk <boris.popadiuk@gmail.com>
2018-05-02 15:49:48 -0700

    change comments for goertzel.py for clarity

--- Goertzel/goertzel.py
+++ Goertzel/goertzel.py
@@ -2,12 +2,12 @@
 Implementation of the Goertzel Algorithm, used here to compute the relative 
 magnitude of each frequency on the keyboard
 
-Source for algorithm: Kevin Banks
+Code based on pseudocode in this article:
 https://www.embedded.com/design/configurable-systems/4024443/The-Goertzel-Algorithm
 
-SAMPLE_RATE and WINDOW_SIZE are still works in progress. More research needs to
-be done to identify optimal values. For now they're more or less voodoo constants
-that seem to work the best for me thusfar. 
+SAMPLE_RATE and WINDOW_SIZE are still works in progress. I need to do more research to
+identify optimal values. For now they're more or less voodoo constants
+that seem to work the best for frequency resolution thus far. 
 """
 
 import math
